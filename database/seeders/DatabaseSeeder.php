87a2104cc00c767e2a0ae13978c192f45325f2eb
SajaAltrad <sajaaltrad01@gmail.com>
2026-07-10 21:52:30 +0300

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,7 +18,6 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             AdminUserSeeder::class,
             LocationSeeder::class,
-            DemoDataSeeder::class,
         ]);
     }
 }
